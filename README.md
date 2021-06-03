2bf70758491d8917b67175ad605b1ff2e9740155
Charlie Black <cblack@pivotal.io>
2021-06-03 09:28:52 -0700

    altered by adding and deleting things.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # github-1
+## deconf
+### nect
 ![alt text](images/bottle.jpg)
 ![alt text](images/window.jpg)
 hi
--- src/main/java/org/hth/internship/InternshipApplication.java
+++ src/main/java/org/hth/internship/InternshipApplication.java
@@ -1,17 +1,68 @@
 package org.hth.internship;
 
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.client.ClientCache;
+import org.apache.geode.cache.client.ClientCacheFactory;
+import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class InternshipApplication {
+    public static void main(String[] args) {
+        // main2(args);
+        ClientCache cache = new ClientCacheFactory()
+                .addPoolLocator("localhost", 10334)
+                .create();
+        Region<String, String> region = cache
+                .<String, String>createClientRegionFactory(ClientRegionShortcut.PROXY)
+                .create("goat");
 
-	public static void main(String[] args) {
+        //loop over all keys on the servers and print out the keys and values.
+        for (Object currentkey : region.keySetOnServer()) {
+            System.out.println("currentkey = " + currentkey);
+            System.out.println("region.get(currentkey) = " + region.get(currentkey));
+        }
+        //the command will send the keys and values up into a sever/region where it is stored .
+        region.put("octopus", asciiart.octopus);
+        region.put("tree", asciiart.tree);
+        //pringint out what ever is nicole
+        System.out.println("region.get(\"nicole\") = " + region.get("nicole"));
+        for (Object currentkey : region.keySetOnServer()) {
+            System.out.println("currentkey = " + currentkey);
+            System.out.println("region.get(currentkey) = " + region.get(currentkey));
+        }
 
-		System.out.println("InternshipApplication.main");
-		for(int i = 0; i < 100; i=i+1) {
-			System.out.println("Hi Nicole! " + i);
-		}
-	}
+        //loop forever
+        while (true) {
+            //read input from user
+            Scanner scanner = new Scanner(System.in);
+            String userinput = scanner.nextLine();
+            //print out what the user typed and it's value
+            System.out.println("userinput = " + userinput);
+            System.out.println("region.get(userinput) = " + region.get(userinput));
+        }
+    }
 
+    // the string allows the code to be run over and over again.
+    public static void main2(String[] args) {
+
+        for (long i = 1; i <= 50; i = i + 1) {
+
+            System.out.println(asciiart.squerl);
+            System.out.println(asciiart.tree);
+            System.out.println(asciiart.doge);
+            System.out.println(asciiart.frog);
+            System.out.println(asciiart.octopus);
+            System.out.println(asciiart.chicken);
+            System.out.println(asciiart.frog2);
+            System.out.println(asciiart.house);
+            System.out.println("froghat = " + asciiart.froghat);
+
+        }
+    }
 }
+
